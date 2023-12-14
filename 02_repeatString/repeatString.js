ad3b9ea5e6afe5650c329ac3638f134da0539633
soniawu <sonia.wu.0906@gmail.com>
2023-12-13 20:04:47 -0600

    Finish 02-repeatString testing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(inString, num) {
+    // return Error if num is a negative number
+    if (num<0) return "ERROR";
 
+    //handle normal condition
+    let builtString ="";
+    for (let i=1; i<=num; i++) {
+        builtString += inString;
+    }
+    return builtString;
 };
 
 // Do not edit below this line
