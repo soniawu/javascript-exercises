74e34017e95573d6d0de086f08a99a843d708a77
soniawu <sonia.wu.0906@gmail.com>
2023-12-13 21:46:30 -0600

    Finish 05_sumAll changes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    if ((typeof(arg1)!=="number") || (typeof(arg2)!=="number") ||
+        (arg1<0) || (arg2<0)) 
+        return "ERROR";
 
+    let lower = 0;
+    let upper = 0;
+    let sum = 0;
+    if (arg1<arg2) {
+        lower = arg1;
+        upper = arg2;
+    } else {
+        lower = arg2; 
+        upper = arg1;
+    }
+
+    for (let i=lower; i<=upper; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
