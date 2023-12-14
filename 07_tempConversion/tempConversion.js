5a293878c625fcacbb556311f89dc05a878ebd25
soniawu <sonia.wu.0906@gmail.com>
2023-12-13 22:25:10 -0600

    Finish 07_tempConversion changes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  // c = (f-32) X 5/9
+  let c = (f -32) * (5 / 9);
+  return (Math.round(c * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  // f = c X 9/5 + 32
+  let f = c * (9/5) + 32;
+  return (Math.round(f * 10) / 10);
 };
 
 // Do not edit below this line
