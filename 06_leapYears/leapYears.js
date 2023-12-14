f66883258370b29d834dedd72f5990eea9c0c30b
soniawu <sonia.wu.0906@gmail.com>
2023-12-13 22:02:18 -0600

    Finish 06_leapYears changes

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // 1) case of divisible by 4 but not by 100
+    if ((year % 4 === 0) && (year % 100 !== 0)) {
+        return true;
+    // 2) case divisible by 4 and 100 and 400 
+    } else if ((year % 4 === 0) && (year % 100 === 0) && (year % 400 === 0)) {
+        return true;
+    // 3) every year not included in case 1) or 2)
+    } else {
+        return false;
+    }
 
 };
 
