f58be5f9c5678eb4e9bf10ef0d1fa6636d77952c
soniawu <sonia.wu.0906@gmail.com>
2023-12-13 20:17:19 -0600

    Finish 03-reverseString changes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inString) {
+    let revString = "";
+    for (let i=(inString.length-1); i>=0; i--) {
+        revString += inString[i];
+    }
+    return revString;
 };
 
 // Do not edit below this line
