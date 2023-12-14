a112c60fe820834a68805fd9ee31a203561307ee
soniawu <sonia.wu.0906@gmail.com>
2023-12-13 21:27:02 -0600

    Finish 04-removeFromArray changes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
 const removeFromArray = function() {
+let arg = arguments.length;
 
+// although arguments length less than 2 is not in the test, include here.
+if (arg<2) {
+    return "ERROR";
+}
+
+let inArr = arguments[0];
+// loop through the list of elements to be removed
+for (let i=1; i<arg; i++) {
+    let thisElm = arguments[i];
+    let idx = inArr.indexOf(thisElm);
+    while (idx>-1) {
+        inArr.splice(idx,1);
+        idx = inArr.indexOf(thisElm);
+    }
+}
+
+return inArr;
 };
 
 // Do not edit below this line
