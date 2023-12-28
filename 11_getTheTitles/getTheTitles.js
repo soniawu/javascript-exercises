aff758e4c3305eff640b7a3fd11e5ceb77036abd
soniawu <sonia.wu.0906@gmail.com>
2023-12-27 19:42:57 -0600

    Finish 11-getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+let bookArr = [];
+books.forEach(book => { bookArr.push(book.title)
+});
+return bookArr;
 };
 
 // Do not edit below this line
