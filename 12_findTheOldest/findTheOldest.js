c8fe6d7baff047148591368e1c240d12752d1f66
soniawu <sonia.wu.0906@gmail.com>
2023-12-27 20:20:10 -0600

    Finish 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentyear = (new Date()).getFullYear();
+    let lastAge = 0;
+    let age = 0;
+    let oldest = [];
 
+    people.forEach(person => {
+        let bYear = person.yearOfBirth;
+        let dYear = currentyear;
+        if (typeof person.yearOfDeath !== "undefined") {dYear = person.yearOfDeath}
+        age = dYear - bYear + 1;
+        if (age > lastAge) {
+            oldest.pop();
+            oldest.push(person);
+            lastAge = age;
+        }
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
